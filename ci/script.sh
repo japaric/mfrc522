5e63ee4de3fb17ce091137697e0547880aee6032
Jorge Aparicio <jorge@japaric.io>
2018-05-16 18:11:15 +0200

    remove linux-embedded-hal from Cargo.toml

--- ci/script.sh
+++ ci/script.sh
@@ -2,6 +2,19 @@ set -euxo pipefail
 
 main() {
     cargo check --target $TARGET
+
+    case $TARGET in
+        armv*)
+            cat >> Cargo.toml <<'EOF'
+[dev-dependencies]
+linux-embedded-hal = "0.2.0"
+EOF
+
+            cargo check --target $TARGET --examples
+            ;;
+        *)
+            ;;
+    esac
 }
 
 main
